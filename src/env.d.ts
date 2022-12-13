ad4075cb24c3ef09291471e3293fd00517b3871f
yuto_yy <yuto.yoshioka@trq.co.jp>
2022-12-13 17:46:23 +0900

    追加　contentful

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,4 +2,9 @@ interface ImportMetaEnv {
   readonly CONTENTFUL_SPACE_ID: string;
   readonly CONTENTFUL_DELIVERY_TOKEN: string;
   readonly CONTENTFUL_PREVIEW_TOKEN: string;
+  readonly DEV: any;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
